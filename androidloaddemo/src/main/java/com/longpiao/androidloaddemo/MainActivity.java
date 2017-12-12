142f188b6ac9f70c330cc67876f78110f33101fc
narkang <18271680922@163.com>
2017-12-12 17:21:24 +0800

    galleryfragment

--- androidloaddemo/src/main/java/com/longpiao/androidloaddemo/MainActivity.java
+++ androidloaddemo/src/main/java/com/longpiao/androidloaddemo/MainActivity.java
@@ -1,13 +1,34 @@
 package com.longpiao.androidloaddemo;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.longpiao.androidloaddemo.view.GalleryFragment;
+import com.narkang.util.mvp.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onBindView(@Nullable Bundle savedInstanceState) {
+        findViewById(R.id.tv_choose_pic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new GalleryFragment().show(getSupportFragmentManager(), GalleryFragment.class.getName());
+            }
+        });
     }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
 }
---
+++ androidloaddemo/src/main/java/com/longpiao/androidloaddemo/view/GalleryView.java
@@ -0,0 +1,18 @@
+package com.longpiao.androidloaddemo.view;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by chenxk on 2017/12/12.
+ */
+
+public class GalleryView extends RecyclerView{
+
+    public GalleryView(Context context) {
+        super(context);
+    }
+
+
+
+}
