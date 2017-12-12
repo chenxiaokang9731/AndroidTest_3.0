15b8a79e51fa48689622086816733bd7f3f84f64
narkang <18271680922@163.com>
2017-12-12 16:47:10 +0800

    adD

--- util/src/main/java/com/longbill/util/mvp/base/BasePresenter.java
+++
@@ -1,8 +0,0 @@
-package com.longbill.util.mvp.base;
-
-/**
- * Created by Administrator on 2017/11/28.
- */
-
-public class BasePresenter {
-}
--- util/src/main/java/com/longbill/util/type/TUtil.java
+++
@@ -1,8 +0,0 @@
-package com.longbill.util.type;
-
-/**
- * Created by Administrator on 2017/11/28.
- */
-
-public class TUtil {
-}
